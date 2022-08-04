b10e64bd7a601a82d6475143becbf7da03456b22
plaski <taskila.dd@gmail.com>
2022-08-04 08:53:47 +0300

    E2E - Stations
    E2E tests for stations page

--- cypress/e2e/stations.cy.js
+++ cypress/e2e/stations.cy.js
@@ -12,4 +12,105 @@ describe("E2E - stations [with datasets]", () =>
         cy.contains('City-bike-app', { timeout: queryTimeout }).should('be.visible');
     });
 
+    // Test pagescrolls
+    const arr_pref = ['next', 'previous'];
+    const arr_pages = ['next', 'prev'];
+
+    for (let i = 0; i <= 1; i++) 
+    {
+        it("should load " + arr_pref[i] + " page", () => {
+            cy.viewport(1120, 1000);
+
+            cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+
+            cy.get('[data-cy="'+arr_pages[i]+'"]', { timeout: queryTimeout })
+            .should('have.css', 'color').and('eq', 'rgb(100, 100, 100)');
+
+            cy.get('[data-cy="'+arr_pages[i]+'"]').click();
+            cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+            cy.get('[data-cy="expand"]', { timeout: queryTimeout }).should('be.visible');
+            cy.get('[data-cy="no_res"]').should('not.exist');
+        });
+    }
+    
+    // Go to the last page 
+    it("should load last page", () => {
+        cy.viewport(1120, 1000);
+
+        // Wait for pageload
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+        
+        // Button for last page should be available
+        cy.get('[data-cy="last"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgb(61, 61, 61)');
+        
+        // Click and wait for load
+        cy.get('[data-cy="last"]').click();
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+        
+        // Button should be disabled
+        cy.get('[data-cy="last"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgba(161, 21, 21, 0.67)');
+        cy.get('[data-cy="expand"]', { timeout: queryTimeout }).should('be.visible');
+        cy.get('[data-cy="no_res"]').should('not.exist');
+    });
+
+    // Check that previous page works on last page
+    it("should load previous page after last page", () => {
+        cy.viewport(1120, 1000);
+
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+
+        cy.get('[data-cy="prev"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgb(100, 100, 100)');
+
+        cy.get('[data-cy="prev"]').click();
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+        cy.get('[data-cy="expand"]', { timeout: queryTimeout }).should('be.visible');
+        cy.get('[data-cy="no_res"]').should('not.exist');
+    });
+
+    // Check that we can go back to last page 
+    it("should load last page back", () => {
+        cy.viewport(1120, 1000);
+
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+
+        cy.get('[data-cy="next"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgb(100, 100, 100)');
+
+        cy.get('[data-cy="next"]').click();
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+        cy.get('[data-cy="expand"]', { timeout: queryTimeout }).should('be.visible');
+
+        // Button should be disabled since we should be on the last page again
+        cy.get('[data-cy="last"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgba(161, 21, 21, 0.67)');
+        cy.get('[data-cy="no_res"]').should('not.exist');
+    });
+
+    it("should load first page", () => {
+        cy.viewport(1120, 1000);
+
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+
+        cy.get('[data-cy="first"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgb(61, 61, 61)');
+
+        cy.get('[data-cy="first"]').click();
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible');
+        cy.get('[data-cy="expand"]', { timeout: queryTimeout }).should('be.visible');
+
+        // Button should be disabled since we are on the first page
+        cy.get('[data-cy="first"]', { timeout: queryTimeout })
+        .should('have.css', 'color').and('eq', 'rgba(161, 21, 21, 0.67)');
+        cy.get('[data-cy="no_res"]').should('not.exist');
+    });
+
+    it("should find searched term", () => {
+        cy.viewport(1120, 1000);
+
+        cy.get('[data-cy="srch"]', { timeout: queryTimeout }).should('be.visible').type('pasila{enter}');
+        cy.contains('Pasilan asema', { timeout: queryTimeout });
+    });
 });
